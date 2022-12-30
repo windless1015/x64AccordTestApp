bc00337586293b355f748d09cc91920b05739730
windless1015 <shangtao1989@qq.com>
2022-12-30 15:19:58 +0700

    add the HttpUploadFile.cs

---
+++ HttpUploadFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ requirement: 
+Adding some apis into this classs to implement the requirement of uploading files(inlcuding jpg and mp4 files) to remote servers
+requirements: refer to the page https://www.showdoc.com.cn/p/a5ae4fbc28b97a9add00fc6dcc79154e
+ */
+namespace x64AccordTestApp
+{
+    internal class HttpUploadFile
+    {
+
+    }
+}
